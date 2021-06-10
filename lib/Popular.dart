81719c52225daaa632155688805428a947c55fd0
patelharsh15 <harshbpatel365.hp@gmail.com>
2021-06-10 18:16:29 +0530

    FirebaseFirestore

--- lib/Popular.dart
+++ lib/Popular.dart
@@ -4,42 +4,52 @@ import 'package:flutter_appgovernment_subsidy/model/PopularItem.dart';
 import 'package:get/get.dart';
 
 import 'popularListItem.dart';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+final subsidies = FirebaseFirestore.instance.collection("subsidy");
+
 class Popular extends StatefulWidget {
   @override
   _PopularState createState() => _PopularState();
 }
 
 class _PopularState extends State<Popular> {
+  var subsidyName;
+  var subsidyInfo;
 
-
-List<PopularItem> popularList=AppList.popularItemList;
+  List<PopularItem> popularList = AppList.popularItemList;
 
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
+    getAllSubsidies();
   }
 
+  getAllSubsidies() async {
+    print("1");
+    DocumentSnapshot dataSnapshot = await subsidies.doc("subsidy 1").get();
+    setState(() {
+      subsidyName = dataSnapshot["name"];
+      subsidyInfo = dataSnapshot["info"];
+    });
+    print("2");
+    print(dataSnapshot["name"]);
+  }
 
   @override
   Widget build(BuildContext context) {
     return Container(
-
-          height: Get.height,
-          width: Get.width,
+      height: Get.height,
+      width: Get.width,
       child: GridView.builder(
-
-        gridDelegate: new SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2,crossAxisSpacing: 10),
-          itemCount: popularList.length,
-        itemBuilder: (BuildContext context,int index){
-          return popularListItem(popularList,index);
+        gridDelegate: new SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2, crossAxisSpacing: 10),
+        itemCount: 1,
+        itemBuilder: (BuildContext context, int index) {
+          return popularListItem(popularList, index, subsidyName, subsidyInfo);
         },
-
-
       ),
-
     );
   }
 }
-
-
